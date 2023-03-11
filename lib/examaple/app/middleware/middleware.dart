ae29300d1e2eee6498b408446c2fcb4a3c169b11
CreVL <minnibaev.vlad@mail.ru>
2023-03-11 19:39:08 +0300

    http-hand

---
+++ lib/examaple/app/middleware/middleware.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'dart:convert';
+
+Future<void> main() async {
+  final server = await HttpServer.bind(InternetAddress.anyIPv4, 8484);
+  print('Listening on ${server.address}:${server.port}');
+
+  await for (HttpRequest request in server) {
+    if (request.method == 'GET' && request.uri.path == '/cloud') {
+      final response = request.response;
+      response.statusCode = HttpStatus.ok;
+      response.headers.contentType = ContentType.text;
+      response.write('Люблю облака');
+      await response.close();
+    } else {
+      final response = request.response;
+      response.statusCode = HttpStatus.notFound;
+      response.headers.contentType = ContentType.text;
+      response.write(':(');
+      await response.close();
+    }
+  }
+}
+
